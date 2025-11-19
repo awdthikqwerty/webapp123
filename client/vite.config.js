ccfcf570b0b1fc09de38d1783ecabeb4dbdad44e
awdthikqwerty <155189554+awdthikqwerty@users.noreply.github.com>
2025-11-19 15:40:20 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite';
-import React from "react";
-
+import react from '@vitejs/plugin-react';  // ✅ импортируем плагин
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],          // ✅ используем его здесь
 });
-
-
-
